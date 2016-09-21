5bb953718a1baa2d661e345103b32e94a190b783
asinuk-sinyuk <sinuykal@gmail.com>
2016-09-21 11:06:53 +0300

    resolved conflicts

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-//import util.WebDriverHelper;
+import util.WebDriverHelper;
 
 import java.io.IOException;
 
