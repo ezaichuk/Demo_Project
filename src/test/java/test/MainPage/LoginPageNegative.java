f6e0348dc7664c176d37bd8b809b3cf6355ecc65
asinuk-sinyuk <sinuykal@gmail.com>
2016-09-21 17:01:43 +0300

    Added positive/negative tests for Password recovery

--- src/test/java/test/MainPage/LoginPageNegative.java
+++ src/test/java/test/MainPage/LoginPageNegative.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.MyAccountPage;
 import ru.yandex.qatools.allure.annotations.Step;
 import test.BaseTest;
 import util.WebDriverHelper;
@@ -15,10 +16,12 @@ import java.io.IOException;
 public class LoginPageNegative extends BaseTest{
 
     private LoginPage loginPage;
+    private MyAccountPage myAccountPage;
 
     @BeforeMethod
     public void initPageObjects() {
         loginPage = PageFactory.initElements(driver, LoginPage.class);
+        myAccountPage = PageFactory.initElements(driver, MyAccountPage.class);
     }
 
     @Test
@@ -41,13 +44,23 @@ public class LoginPageNegative extends BaseTest{
     @Step
     public void PasswordRecovery () {
         //placeholder
-        Assert.assertTrue(false, "TODO: add password recovery test");
+        loginPage.LostPasswordClick();
+        Assert.assertTrue(myAccountPage.entryTitle.getText().contains("Lost Password"), "Verify user got redirected on Lost Password page.");
+
+        myAccountPage.inputUsernameEmail.sendKeys("1111");
+        myAccountPage.buttonResetPassword.click();
+        Assert.assertTrue(myAccountPage.errorMessage.getText().contains("Invalid username or e-mail."), "Verify user can\'t get password for nonexistent account.");
+
+        myAccountPage.inputUsernameEmail.sendKeys("QA");
+        myAccountPage.buttonResetPassword.click();
+        Assert.assertTrue(myAccountPage.validMessage.getText().contains("Password reset email has been sent."), "Verify password reset email has been sent.");
     }
 
     @Test
     @Step
     public void InvalidUsername (){
         //ERROR: Invalid username.
+        loginPage.Open(baseUrl);
         loginPage.LoginAs("1111", "1111");
         WebDriverHelper.WaitTillVisible(loginPage.loginErrorMessage, driver);
         System.out.println(loginPage.loginErrorMessage.getText());
