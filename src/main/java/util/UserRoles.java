dbd96d0841af1338d56e2d1bf2feb097d1af4aee
asinuk-sinyuk <sinuykal@gmail.com>
2016-09-29 11:04:14 +0300

    Fixed UserRoles.java, fixed ProductsPage.java. Blockers

--- src/main/java/util/UserRoles.java
+++ src/main/java/util/UserRoles.java
@@ -2,28 +2,22 @@ package util;
 
 public enum UserRoles {
 
-    SUBSCRIBER("subscriber","Subscriber"),
-    SHOP_MANAGER("show_manager", "Менеджер магазина"),
-    CUSTOMER("customer","Клиент"),
-    CONTRIBUTOR("contributor","Contributor"),
-    AUTHOR("author","Author"),
-    EDITOR("editor","Editor"),
-    ADMINISTRATOR("administrator", "Administrator");
+    SUBSCRIBER("subscriber"),
+    SHOP_MANAGER("shop_manager"),
+    CUSTOMER("customer"),
+    CONTRIBUTOR("contributor"),
+    AUTHOR("author"),
+    EDITOR("editor"),
+    ADMINISTRATOR("administrator");
 
     private String value;
-    private String rolename;
 
-    UserRoles(String value, String rolename) {
+    UserRoles(String value) {
         this.value = value;
-        this.rolename = rolename;
     }
 
     public String value() {
         return value;
-    }
-
-    public String role(){
-        return rolename;
     }
 
 }
