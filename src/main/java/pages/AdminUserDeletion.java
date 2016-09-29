9ea903af15a25399614c6b9cae75a43f9e612992
alexey-sinyuk <sinuykal@gmail.com>
2016-09-29 13:59:02 +0300

    Added tests for User management. (#14)
    * Adding tests for user creation
    
    Added user creation tests
    
    meanwhile
    
    Removed redundant variables, methods for mouseover. Changed xmls to run tests separate by owner
    
    Moved test xmls to folder
    
    resolved conflicts
    
    Adding tests for user creation
    
    Added user creation tests
    
    User creation tests
    
    Added User Creation tests and methods for page object
    
    * fixed highlight, uncommented testng.xml
    
    * Adding tests for user creation
    
    Added user creation tests
    
    meanwhile
    
    Removed redundant variables, methods for mouseover. Changed xmls to run tests separate by owner
    
    Moved test xmls to folder
    
    resolved conflicts
    
    Adding tests for user creation
    
    Added user creation tests
    
    User creation tests
    
    Added User Creation tests and methods for page object
    
    fixed highlight, uncommented testng.xml
    
    * Adding tests for user creation
    
    Added user creation tests
    
    meanwhile
    
    Removed redundant variables, methods for mouseover. Changed xmls to run tests separate by owner
    
    Moved test xmls to folder
    
    resolved conflicts
    
    Adding tests for user creation
    
    Added user creation tests
    
    User creation tests
    
    Added User Creation tests and methods for page object
    
    * Adding tests for user creation
    
    Added user creation tests
    
    meanwhile
    
    Removed redundant variables, methods for mouseover. Changed xmls to run tests separate by owner
    
    Moved test xmls to folder
    
    resolved conflicts
    
    Adding tests for user creation
    
    Added user creation tests
    
    User creation tests
    
    Added User Creation tests and methods for page object
    
    fixed highlight, uncommented testng.xml
    
    user creation for each role
    
    'Users of all roles created' test finished
    
    * modified UserRoles enum, removed redundant values
    
    * Adding tests for user creation
    
    Added user creation tests
    
    meanwhile
    
    Removed redundant variables, methods for mouseover. Changed xmls to run tests separate by owner
    
    Moved test xmls to folder
    
    resolved conflicts
    
    Adding tests for user creation
    
    Added user creation tests
    
    User creation tests
    
    Added User Creation tests and methods for page object
    
    * Adding tests for user creation
    
    Added user creation tests
    
    meanwhile
    
    Removed redundant variables, methods for mouseover. Changed xmls to run tests separate by owner
    
    Moved test xmls to folder
    
    resolved conflicts
    
    Adding tests for user creation
    
    Added user creation tests
    
    User creation tests
    
    Added User Creation tests and methods for page object
    
    fixed highlight, uncommented testng.xml
    
    user creation for each role
    
    'Users of all roles created' test finished
    
    * modified UserRoles enum, removed redundant values
    
    * user deletion implementation progress
    
    * fixed Bolgarov.xml
    
    * uncommenred Sinyukxml
    
    * added test for single and multiply user deletion

---
+++ src/main/java/pages/AdminUserDeletion.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class AdminUserDeletion extends Page {
+
+    @FindBy(how = How.ID, using = "submit")
+    public WebElement submitButton;
+
+    @FindBy(how = How.XPATH, using = ".//*[@id='updateusers']/div/ul/li")
+    public WebElement usersToDelete;
+
+    public void SubmitDeletion (){
+        submitButton.click();
+    }
+
+    public AdminUserDeletion(WebDriver driver) {
+        super(driver);
+    }
+}
--- src/main/java/pages/AdminUserManagement.java
+++ src/main/java/pages/AdminUserManagement.java
@@ -1,15 +1,82 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
+import util.WebDriverHelper;
+import java.util.List;
 
 public class AdminUserManagement extends Page {
 
     @FindBy(xpath = ".//a[@class='page-title-action']")
     public WebElement addNewElement;
 
+    @FindBy(how = How.ID, using = "user-search-input" )
+    public WebElement userSearch;
+
+    @FindBy(how = How.ID, using = "search-submit")
+    public WebElement searchButton;
+
+    @FindBys({@FindBy (xpath = ".//tbody[@id='the-list']/tr")})
+    public List<WebElement> userList;
+
+    @FindBy(how = How.ID, using = "cb-select-all-1")
+    public WebElement selectAllCheckbox;
+
+    @FindBy(how = How.ID, using = "bulk-action-selector-top")
+    public WebElement bulkActions;
+
+    @FindBy(how = How.ID, using = "doaction")
+    public WebElement submitActionButton;
+
+    @FindBy(how = How.XPATH, using = ".//*[@id='message']/p")
+    public WebElement message;
+
+    @FindBy(how = How.XPATH, using = ".//*[@id='the-list']/tr/td")
+    public WebElement searchMessage;
+
+    public void DoSearch (String search){
+        userSearch.clear();
+        userSearch.sendKeys(search);
+        searchButton.click();
+    }
+
     public AdminUserManagement(WebDriver driver) {
         super(driver);
     }
+
+    public boolean DeleteUser(String username) {
+        WebElement userlink;
+        WebElement delete;
+        DoSearch(username);
+        if (userList.size() != 1) {
+            return false;
+        }else{
+            selectAllCheckbox.click();
+            new Select(bulkActions).selectByValue("delete");
+            submitActionButton.click();
+            /*userlink = userList.get(0).findElement(By.xpath(".//td[@data-colname='Username']"));
+            WebDriverHelper.MouseOver(userlink, driver);
+            delete = userList.get(0).findElement(By.xpath(".//td[@data-colname='Username']/div/span[2]/a"));
+            delete.click();*/
+            return true;
+        }
+    }
+
+    public boolean DeleteUsers(String filter) {
+        DoSearch(filter);
+        if (userList.size() == 0) {
+            return false;
+        }else{
+            selectAllCheckbox.click();
+            new Select(bulkActions).selectByValue("delete");
+            submitActionButton.click();
+            return true;
+        }
+    }
+
 }
